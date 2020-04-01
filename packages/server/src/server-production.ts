35a91f979371ce62f704ead1fb7a7c41f176bf93
sunhao <imsunhao@vip.163.com>
2020-04-01 15:32:38 +0800

    111

--- packages/server/src/server-production.ts
+++ packages/server/src/server-production.ts
@@ -1,7 +1,8 @@
 import { ServerStart } from './type'
 import { Service, APP } from './utils'
 
-export function start({ server, setting, dll, injectContext }: ServerStart) {
+export function start({ server, setting, dll, injectContext, port }: ServerStart) {
+  process.env.PORT = port as string
   Service.updateInjectContext(injectContext)
 
   const service = new Service(server, setting, new APP(), dll)
